69c47a4563b52372f6915cf71ae56c53bdfa3907
Kevin van Dijk <kevin@sqre.one>
2020-11-13 18:48:12 +0100

    Add empty 'select deadline' screen

--- app/App.js
+++ app/App.js
@@ -5,6 +5,7 @@ import { createStackNavigator } from '@react-navigation/stack';
 import Home from './screens/Home';
 import SetGoal from './screens/SetGoal';
 import AddMoney from './screens/AddMoney';
+import SelectDeadline from './screens/SelectDeadline';
 import SelectCheckupBuddy from './screens/SelectCheckupBuddy';
 import PaymentReceived from './screens/PaymentReceived';
 
@@ -27,6 +28,7 @@ const App: () => React$Node = () => {
         <Stack.Screen name="Home" component={Home} />
         <Stack.Screen name="SetGoal" component={SetGoal} />
         <Stack.Screen name="AddMoney" component={AddMoney} />
+        <Stack.Screen name="SelectDeadline" component={SelectDeadline} />
         <Stack.Screen
           name="SelectCheckupBuddy"
           component={SelectCheckupBuddy}
--- app/screens/SelectDeadline.js
+++ app/screens/SelectDeadline.js
@@ -4,27 +4,26 @@ import { SafeAreaView, View } from 'react-native';
 import { ScreenStyles as styles } from './styles';
 import Button from '../components/Button/Button';
 import ScreenTitle from '../components/ScreenTitle/ScreenTitle';
-import MoneySelector from '../components/MoneySelector/MoneySelector';
 
-const AddMoney = ({ navigation }) => {
+const SelectDeadline = ({ navigation }) => {
   return (
     <SafeAreaView style={styles.safeContainer}>
       <View style={styles.container}>
         <View style={styles.header}>
           <ScreenTitle
-            mainText="Hoeveel geld zet je in?"
+            mainText="Wanneer moet je doel gehaald zijn?"
             subText="Je krijgt je geld alleen terug als je je doel hebt behaald."
           />
         </View>
         <View style={styles.body}>
-          <MoneySelector />
+          <View />
         </View>
         <View style={styles.footer}>
           <Button onPress={navigation.goBack} inverted label="Vorige" />
           <View style={styles.footerSpacer} />
           <Button
             label="Volgende"
-            onPress={() => navigation.navigate('SelectDeadline')}
+            onPress={() => navigation.navigate('SelectCheckupBuddy')}
           />
         </View>
       </View>
@@ -32,4 +31,4 @@ const AddMoney = ({ navigation }) => {
   );
 };
 
-export default AddMoney;
+export default SelectDeadline;
